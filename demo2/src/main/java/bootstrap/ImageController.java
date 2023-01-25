254c7f2f66ed80da3da514d4ded6945e95703d06
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-01-25 14:50:41 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/ImageController.java
+++ demo2/src/main/java/bootstrap/ImageController.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ import domain.Image;
 
 import repositories.ImageDbRepository;
 
-@RestController
+@Controller
 public class ImageController {
 
 	@Autowired
--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -1,9 +1,17 @@
 package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
+@EnableJpaRepositories("repositories") 
 @SpringBootApplication
+@ComponentScan({"bootstrap"})
+@EntityScan("domain")
 public class Demo2Application {
 
 	public static void main(String[] args) {
