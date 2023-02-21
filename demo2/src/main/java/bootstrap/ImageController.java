888646de11d14dac9d41065358c5f61df0ea5029
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-02-21 18:09:41 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/ImageController.java
+++ demo2/src/main/java/bootstrap/ImageController.java
@@ -295,6 +295,7 @@ public class ImageController {
 		imageDbRepository.save(imageToAdd);
 		System.out.print("test "+path.getPath());
 		System.out.println("Name: "+imageToAdd.getName());
+		System.out.println("Album: "+imageToAdd.getAlbum().getName());
 		
 		
 		
