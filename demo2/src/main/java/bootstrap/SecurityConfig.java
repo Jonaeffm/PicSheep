daf37ed307f748a88dbe7f0b4cb74410d3b5a57d
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-02-23 16:19:51 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/SecurityConfig.java
+++ demo2/src/main/java/bootstrap/SecurityConfig.java
@@ -1,17 +1,24 @@
 package bootstrap;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
-	  protected void configure(HttpSecurity http) throws Exception {
-	        http.csrf().disable();
-	    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http.csrf()
+	      .disable();
+	     
+
+	    return http.build();
+	}
 
 }
