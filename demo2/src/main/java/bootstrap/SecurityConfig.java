6a1002fe583005f34ca5521d9de2780d468b68a9
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-02-23 16:35:27 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/SecurityConfig.java
+++ demo2/src/main/java/bootstrap/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	
-		 http.authorizeHttpRequests().requestMatchers("/*").permitAll();
+		
 	    return http.build();
 	}
 
