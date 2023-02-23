2255c368faeacb2fef577f7d4bd0fff7500f9af4
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-02-23 15:51:35 +0100

    Add files via upload

---
+++ demo2/src/main/java/bootstrap/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package bootstrap;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .csrf().disable();
+      
+  }
+}
