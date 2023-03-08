5590ab5b7fb0cf4fac92b788674d4d48bf7f3e1a
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-03-08 19:07:27 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/SecurityConfig.java
+++ demo2/src/main/java/bootstrap/SecurityConfig.java
@@ -14,11 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http.csrf()
-	      .disable();
-	     
-
+		http.csrf().disable()
+        
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+		.and()
+	      .formLogin();
+		
 	    return http.build();
 	}
+	
+	
 
 }
