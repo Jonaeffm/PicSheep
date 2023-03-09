f2a14b39355e4575c816aaaa3b5817dd8e09c582
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-03-09 15:48:13 +0100

    Add files via upload

--- demo2/src/main/java/bootstrap/SecurityConfig.java
+++ demo2/src/main/java/bootstrap/SecurityConfig.java
@@ -1,10 +1,14 @@
 package bootstrap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -12,27 +16,41 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+	
+	  @Autowired
+	   private UserDetailsService userDetailsService;
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 http.csrf().disable()
+		  http.csrf().disable()
 	   		
 		   
 		   
-         .authorizeRequests()
-         	
-             .anyRequest().authenticated()
-             .and()
-             
-             .formLogin(form -> form
-                     .loginPage("/login")
-                     .defaultSuccessUrl("/")
-                     .failureUrl("/login")
-                     .permitAll()
-                     );
-		
-	    return http.build();
+          .authorizeRequests()
+          	.requestMatchers("/addUser").permitAll()
+              .anyRequest().authenticated()
+              .and()
+              
+              .formLogin(form -> form
+                      .loginPage("/login")
+                      .defaultSuccessUrl("/")
+                      .failureUrl("/login")
+                      .permitAll()
+                      );
+		return http.build();
+	  
 	}
-	
+	 
+	   @SuppressWarnings("deprecation")
+	   @Bean
+	   public static NoOpPasswordEncoder passwordEncoder() {
+	   return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	   }
+
+	   @Autowired
+	   public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
+	       auth.userDetailsService(userDetailsService);
+	   }
+
 	
 
 }
